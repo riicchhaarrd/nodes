ccaa8eecf1f28c84b7990b70772118a4cda4712e
kungfooman <lama12345@gmail.com>
2020-11-30 15:03:54 +0100

    node_vec3: implement freeze/thaw

--- src/node_vec3.js
+++ src/node_vec3.js
@@ -8,12 +8,18 @@ import {prompt_proxy} from "./nodes.js";
 
 class node_vec3 extends node_t
 {
+	/**
+	 * 
+	 */
 	constructor()
 	{
 		super("vec3");
 		this.output = this.addOutput("vec3");
 	}
 	
+	/**
+	 * 
+	 */
 	set_initial_value()
 	{
 		this.output.set_state(
@@ -23,6 +29,36 @@ class node_vec3 extends node_t
 				parseFloat(prompt_proxy())
 			)
 		);
+	}
+
+	/**
+	 * 
+	 */
+	freeze()
+	{
+		var frozenfood = super.freeze();
+		var state = this.output.get_state();
+		frozenfood.extra = {
+			x: state.x,
+			y: state.y,
+			z: state.z
+		}
+		return frozenfood;
+	}
+	
+	/**
+	 * 
+	 * @param {object} o 
+	 */
+	thaw(o)
+	{
+		var tmp = o.extra;
+		if (tmp) {
+			this.output.set_state(
+				new vec3(tmp.x, tmp.y, tmp.z)
+			);
+		}
+		super.thaw(o);
 	}
 };
 
